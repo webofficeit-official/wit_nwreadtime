0ac7ef6b94e67881f75a3d4ca7623bc1709f8a64
Rahul R S <rahul.rs@webofficeit.com>
2023-11-01 17:03:59 +0530

    format update, local lang added

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,18 +1,19 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'Wit Nwreadtime',
-    'description' => 'Wit news extension article readable time counter',
+    'title' => '[WIT] News Read Time',
+    'description' => 'This extension is useful for displaying read time in  news detail view.',
     'category' => 'services',
-    'author' => 'Rahul R S',
-    'author_email' => 'rahul.rs@webofficeit.com',
+    'author' => 'Team WebofficeIT, Rahul R S',
+    'author_email' => 'info@webofficeit.com',
+    'author_company' => 'Weboffice Infotech India Pvt. Ltd.',
     'state' => 'alpha',
     'clearCacheOnLoad' => 0,
-    'version' => '1.0.0',
+    'version' => '1.0.1',
     'constraints' => [
         'depends' => [
             'typo3' => '10.4.0-11.5.99',
-            'news' => '9.4.0-11.0.0',
+            'news' => '9.4.0-11.9.99',
         ],
         'conflicts' => [],
         'suggests' => [],

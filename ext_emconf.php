73ee53626c42056b5c97f1b87384e6b4a580b554
Rahul R S <rahul.rs@webofficeit.com>
2023-11-01 15:43:54 +0530

    package update

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-11.5.99',
+            'typo3' => '10.4.0-11.5.99',
             'news' => '9.4.0-11.0.0',
         ],
         'conflicts' => [],
